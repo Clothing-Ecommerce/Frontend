298eee11706bba1bc954ea842857f5566ada28ac
NguyenHoaiPhong <nguyenhoaiphong14723@gmail.com>
2025-07-15 17:18:05 +0700

    [update] - Create login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import RegisterPage from "./pages/auth/RegisterPage";
-
-// import Login from "./pages/Login";
+import LoginPage from "./pages/auth/LoginPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/auth/login" element={<LoginPage />} />
         <Route path="/auth/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
