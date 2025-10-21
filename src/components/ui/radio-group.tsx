457d023be961247a5b63a057905bef8573691d57
NguyenHoaiPhong <nguyenhoaiphong14723@gmail.com>
2025-10-21 15:32:15 +0700

    [update] - change color radio button

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -14,7 +14,7 @@ const RadioGroupItem = React.forwardRef<HTMLInputElement, React.InputHTMLAttribu
       <input
         type="radio"
         className={cn(
-          "aspect-square h-4 w-4 rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "aspect-square h-4 w-4 rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-[#ff424f] focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 accent-[#ff424f] checked:border-[#ff424f]",
           className,
         )}
         ref={ref}
